f37014d41700fcefc8b9abd73edcb42514127a2f
Sibel Özkaynak <sibelozkaynak@yandex.com.tr>
2018-05-12 22:23:37 +0300

    -Alacarta liste gönderildi.

---
+++ sibelApi/Entity/RestoranAlacarte.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sibelApi.Entity
+{
+    public class RestoranAlacarte
+    {
+        public string code;
+        public string description;
+        public string hotelId;
+        [JsonProperty("params")]
+        public List<AlacarteParams> paramz;
+        public int unitId;
+        public string unitKey;
+    }
+    public class AlacarteParams
+    {
+        public int capacity;
+        public bool childSta;
+        public string desc;
+    }
+}
